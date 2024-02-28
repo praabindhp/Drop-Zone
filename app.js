56a0dcb6849f6026192b9cb3d78e9bd2249bb6b8
praabindhp <praabindhp@gmail.com>
2024-02-28 15:45:34 +0530

    NodeJS Drop-Zone Server

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+
 const uploadRoutes = require('./routes/uploadRoutes');
 const downloadRoutes = require('./routes/downloadRoutes');
+
 require('./config/firebaseConfig');
+const cors = require('cors');
 const app = express();
+app.use(cors());
+
 dotenv.config();
 require('./utils/fileExistence');
 
-// Use routes
+// Use Routes
 app.use('/upload', uploadRoutes);
 app.use('/download', downloadRoutes);
+app.get('/', (req, res) => {
+    res.send(`Howdy 🥷, Welcome To Praabindh's ⚓ Server`);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
